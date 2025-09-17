813d4f5301b08ceb1bedeb026230fc067af99f1e
a3vpro <a3vpro@gmail.com>
2025-09-17 15:23:39 +0200

    Add XML docs for application requisites options

--- Core/Requisites/ApplicationRequisitesOptions.cs
+++ Core/Requisites/ApplicationRequisitesOptions.cs
@@ -2,8 +2,14 @@
 
 namespace VisionNet.Core.Requisites
 {
+    /// <summary>
+    /// Represents configuration options for an application requisite definition.
+    /// </summary>
     public class ApplicationRequisitesOptions
     {
+        /// <summary>
+        /// Gets or sets the type of the requisite that determines how the requirement is evaluated.
+        /// </summary>
         [Browsable(true)]
         [ReadOnly(false)]
         [Category("Requisites")]
@@ -11,6 +17,9 @@ namespace VisionNet.Core.Requisites
         [DisplayName(nameof(Type))]
         public RequisiteType Type { get; set; } = RequisiteType.Custom;
 
+        /// <summary>
+        /// Gets or sets the display name used to identify the requisite within the application configuration.
+        /// </summary>
         [Browsable(true)]
         [ReadOnly(false)]
         [Category("Requisites")]
@@ -18,6 +27,9 @@ namespace VisionNet.Core.Requisites
         [DisplayName(nameof(Name))]
         public string Name { get; set; } = "";
 
+        /// <summary>
+        /// Gets or sets the argument value supplied to the requisite handler, such as command-line parameters or identifiers.
+        /// </summary>
         [Browsable(true)]
         [ReadOnly(false)]
         [Category("Requisites")]
@@ -25,6 +37,9 @@ namespace VisionNet.Core.Requisites
         [DisplayName(nameof(Argument))]
         public string Argument { get; set; } = "";
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the requisite should be evaluated when processing application requirements.
+        /// </summary>
         [Browsable(true)]
         [ReadOnly(false)]
         [Category("Requisites")]
