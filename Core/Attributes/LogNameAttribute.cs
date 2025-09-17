aebe2381a20662b9282c2e5205b07efd88e88b89
a3vpro <a3vpro@gmail.com>
2025-09-17 15:25:43 +0200

    Add XML documentation to LogNameAttribute

--- Core/Attributes/LogNameAttribute.cs
+++ Core/Attributes/LogNameAttribute.cs
@@ -2,15 +2,21 @@
 
 namespace VisionNet.Logging
 {
+    /// <summary>
+    /// Specifies the log name that should be associated with the decorated class when emitting log entries.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class LogNameAttribute: Attribute
     {
+        /// <summary>
+        /// Represents the log name assigned through the attribute to categorize log output.
+        /// </summary>
         public string LogName;
 
-        
-        /// <summary> The LogNameAttribute function is used to set the name of a log file.</summary>
-        /// <param name="string logName"> The name of the log file.</param>
-        /// <returns> The name of the log.</returns>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogNameAttribute"/> class using the provided log name identifier.
+        /// </summary>
+        /// <param name="logName">Log identifier to associate with the decorated class when logging.</param>
         public LogNameAttribute(string logName)
         {
             LogName = logName;
