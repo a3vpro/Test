d1f17595be17e5f32c46c4910f381df9538717cc
a3vpro <a3vpro@gmail.com>
2025-09-18 10:20:39 +0200

    Add English XML docs for StringComparison conversion

--- Core/String/StringComparisonAdapterExtensions.cs
+++ Core/String/StringComparisonAdapterExtensions.cs
@@ -7,6 +7,16 @@ namespace VisionNet.Core.Strings
     /// </summary>
     public static class StringComparisonAdapterExtensions
     {
+        /// <summary>
+        /// Converts an extended string comparison option into the equivalent <see cref="StringComparison"/> value
+        /// so that it can be used with standard .NET string comparison APIs.
+        /// </summary>
+        /// <param name="value">The extended comparison mode to convert. This must be a defined <see cref="StringComparisonEx"/>
+        /// combination describing the culture and case comparison rules to apply.</param>
+        /// <returns>The <see cref="StringComparison"/> value that best represents the culture and case-sensitivity options
+        /// encapsulated by <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="value"/> cannot be translated to a supported
+        /// <see cref="StringComparison"/> value.</exception>
         public static StringComparison Convert(this StringComparisonEx value)
         {
             var adapter = new StringComparisonExAdapter();
